1ba871969d5af2384cdd5592493d1d0fdfc89dd3
Matvey <eaglematvey@gmail.com>
2015-02-28 12:06:58 +0300

    13projectfinal

--- 13project/13project/main.cpp
+++ 13project/13project/main.cpp
@@ -1,20 +1,21 @@
 #include<iostream> //ѕроверить, €вл€етс€ ли контейнер палиндромом.
 #include<fstream>
 #include<vector>
-
+#include<list>
 using namespace std;
 
 template<class T>
-bool isPolynom(const T& v) 
-{  
+bool isPolyndrom(const T& v) 
+{
+	if (v.empty()) return true;
 	T::const_iterator it = v.begin();
-	T::const_reverse_iterator rit = v.rbegin();
-	int halfsize = v.size()/2;
-	for (int i = 1; i <= halfsize; i++, ++it, ++rit)
+	T::const_iterator rit = v.end();
+	rit--;
+	for (it, rit;it<rit ; ++it,--rit)
 		if ((*it) != (*rit))
-			return false;
+			return false;			
 	return true;
-}
+} 
 
 int main()
 {
@@ -23,7 +24,7 @@ int main()
 	int cont1_num;
 	while (fin >> cont1_num)
 		container1.push_back(cont1_num);
-	bool b = isPolynom(container1);
+	bool b = isPolyndrom(container1);
 	cout << boolalpha << b << endl;
 	return 0;
 }
